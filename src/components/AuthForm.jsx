377ac87ca428f5aa1ef92a72879aaee158b33891
minji7901 <kimem7901@gmail.com>
2024-11-27 18:58:06 +0900

    feat : add login,signup page validation function

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import useForm from "../hooks/useForm";
+import { validateForm } from "../utils/validation";
 
 export default function AuthForm({ mode, onSubmit }) {
-  const { formState, onChangeHandler, resetForm } = useForm({
-    id: "",
-    password: "",
-    nickname: mode === "signup" ? "" : undefined,
-  });
+  const { formState, onChangeHandler, resetForm, formErrors } = useForm(
+    {
+      id: "",
+      password: "",
+      nickname: "",
+    },
+    validateForm
+  );
 
   const { id, password, nickname } = formState;
 
@@ -23,28 +27,45 @@ export default function AuthForm({ mode, onSubmit }) {
         value={id}
         onChange={onChangeHandler}
         placeholder="아이디를 입력해주세요ꢙ"
-        className="input-style"
+        className="input-style focus:border-purple-500 focus:shadow-purple-500"
+        minLength={4}
+        maxLength={10}
         required
       />
+      {formErrors.id && (
+        <small className="text-blue-900">{formErrors.id}</small>
+      )}
       <input
         type="password"
         name="password"
         value={password}
         onChange={onChangeHandler}
         placeholder="비밀번호를 입력해주세요ꢙ"
-        className="input-style"
+        className="input-style focus:border-purple-500 focus:shadow-purple-500"
+        minLength={4}
+        maxLength={15}
         required
       />
+      {formErrors.password && (
+        <small className="text-blue-900">{formErrors.password}</small>
+      )}
       {mode === "signup" && (
-        <input
-          type="text"
-          name="nickname"
-          value={nickname}
-          onChange={onChangeHandler}
-          placeholder="닉네임을 입력해주세요 ✩⡱"
-          className="input-style"
-          required
-        />
+        <>
+          <input
+            type="text"
+            name="nickname"
+            value={nickname}
+            onChange={onChangeHandler}
+            placeholder="닉네임을 입력해주세요 ✩⡱"
+            className="input-style focus:border-purple-500 focus:shadow-purple-500"
+            minLength={2}
+            maxLength={10}
+            required
+          />
+          {formErrors.nickname && (
+            <small className="text-blue-900">{formErrors.nickname}</small>
+          )}
+        </>
       )}
       <button type="submit" className="button-style">
         {mode === "login" ? "로그인하기" : "회원가입하기"}
--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -1,15 +1,32 @@
 import { useState } from "react";
 
-export default function useForm(initialState) {
+export default function useForm(initialState, validate) {
   const [formState, setFormState] = useState(initialState);
+  const [formErrors, setFormErrors] = useState({});
 
   const onChangeHandler = (e) => {
     const { name, value } = e.target;
     setFormState((prev) => ({ ...prev, [name]: value }));
+
+    if (validate) {
+      const error = validate(name, value);
+      setFormErrors((prev) => {
+        const updatedErrors = { ...prev };
+
+        if (value.length === 0) {
+          delete updatedErrors[name];
+        } else {
+          updatedErrors[name] = error;
+        }
+
+        return updatedErrors;
+      });
+    }
   };
   const resetForm = () => {
     setFormState(initialState);
+    setFormErrors({});
   };
 
-  return { formState, onChangeHandler, resetForm };
+  return { formState, formErrors, onChangeHandler, resetForm };
 }
